6454418c85e400186a83f9551b7f33cd7671d149
Elenagerman619 <82209704+Elenagerman619@users.noreply.github.com>
2021-05-27 13:45:54 +0300

    Create index-footer.html.php

---
+++ pages/index-footer.html.php
@@ -0,0 +1,14 @@
+<div class="row4" style="margin-top:550px;position: absolute;margin-left: 450px;">
+	<b>©2021 все права защищены</b>
+</div>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+<script>
+let rub = 89;
+let euro = document.getElementById("num1");
+function sum()
+{
+	// console.log("Результат: " +(+euro.value*rub));
+	alert("Стоимость тура в рублях: " +(+euro.value*rub) + " руб");
+}
+</script>
