b35d003c79c03c1d2abef7b32f2f3046d4b29538
Elenagerman619 <82209704+Elenagerman619@users.noreply.github.com>
2021-05-27 13:47:47 +0300

    Create index-menu.html.php

---
+++ pages/index-menu.html.php
@@ -0,0 +1,23 @@
+<div class="row pt-2">
+	<div class="col-1 mx-0 px-0">
+        <a href="http://travel.ru.xsph.ru" style="color: crimson; font-weight:bold; text-decoration:none"><i class="fab fa-fort-awesome" style="font-size:28px"> </i>Travelru</a></div>
+   
+	<div class="col-11">
+		<ul class="nav">
+			<li class="nav-item dropdown"><a class="nav-link dropdown-toggle" data-toggle="dropdown" href="/" role="button" aria-haspopup="true" aria-expanded="false" style="color:grey">Главная</a>
+			<div class="dropdown-menu">
+				<a class="dropdown-item" href="#">О компании</a>
+				<a class="dropdown-item" href="#">Сотрудничество</a>
+				<a class="dropdown-item" href="#">Новости</a>
+				<a class="dropdown-item" href="#">Контакты</a>
+			</div>
+			</li>
+			<li class="nav-item"><a class="nav-link" href="/user.php" style="color:grey">Личный кабинет</a></li>
+			<li class="nav-item"><a class="nav-link" href="#" style="color:grey">Список стран</a></li>
+			<li class="nav-item"><a class="nav-link" href="#" style="color:grey">Каталог отелей</a></li>
+			<li class="nav-item"><a class="nav-link" href="#" style="color:grey">Авиаперелеты</a></li>
+			<li class="nav-item"><a class="nav-link" href="#" style="color:grey">Подбор тура</a></li>
+			<li class="nav-item"><a class="nav-link" href="#" style="color:grey">Памятка</a></li>
+		</ul>
+	</div>
+</div>
