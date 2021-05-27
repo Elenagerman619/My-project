4b5603fbfaee43c99b3821fc13504a83938e16e8
Elenagerman619 <82209704+Elenagerman619@users.noreply.github.com>
2021-05-27 13:46:33 +0300

    Create index-left.html.php

---
+++ pages/index-left.html.php
@@ -0,0 +1,13 @@
+<div>
+	<div class="tel"><i class="fas fa-phone-alt"></i>+7(495)-775-1001</div>
+	<div class="email"><i class="far fa-envelope fa-lg"></i>europe@mail.ru</div>
+</div>	
+<ul class="nav">
+	<li class="nav-item"><a href="detail.php?country=italy" class="nav-link active"><img src="/img/flag-italy.png" width="20" height="20">Италия</a></li>
+	<li class="nav-item"><a href="detail.php?country=spain" class="nav-link"><img src="/img/flag-spain.jpg" width="20" height="20">Испания</a></li>
+	<li class="nav-item"><a href="detail.php?country=greece" class="nav-link" href="http://a0528167.xsph.ru/myproject/greece.html"><img src="/img/flag-greece.jpg" width="20" height="20">Греция</a></li>
+	<li class="nav-item"><a href="detail.php?country=cyprus" class="nav-link" href="http://a0528167.xsph.ru/myproject/cyprus.html"><img src="/img/flag-cyprus.jpg" width="20" height="20">Кипр</a></li>
+	<li class="nav-item"><a href="detail.php?country=france" class="nav-link" href="http://a0528167.xsph.ru/myproject/france.html"><img src="/img/flag-france.jpg" width="20" height="20">Франция</a></li>
+	<li class="nav-item"><a href="detail.php?country=portu" class="nav-link" href="http://a0528167.xsph.ru/myproject/portu.html"><img src="/img/flag-portugalia.png" width="20" height="20">Португалия</a></li>
+	<li class="nav-item"><a href="detail.php?country=chehia" class="nav-link" href="http://a0528167.xsph.ru/myproject/chehia.html"><img src="/img/flag-czech.jpg" width="20" height="20">Чехия</a></li>
+</ul>
