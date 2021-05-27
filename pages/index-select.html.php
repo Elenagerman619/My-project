59ebc2425b6efc64a1091de4a4949890b4b076a7
Elenagerman619 <82209704+Elenagerman619@users.noreply.github.com>
2021-05-27 13:48:50 +0300

    Create index-select.html.php

---
+++ pages/index-select.html.php
@@ -0,0 +1,45 @@
+<form>
+	<div class="row py-3">
+		<h6>Подбор тура</h6>
+		<div class="col">
+			<select class="custom-select">
+            	<option selected>Город вылета</option>
+				<option value="1">Москва</option>
+				<option value="2">Санкт-Петербург</option>
+				<option value="3">Новосибирск</option>
+			</select>        
+		</div>
+        <div class="col">
+			<select class="custom-select">
+				<option selected>Страна прилета</option>
+				<option value="1">Италия</option>
+				<option value="2">Испания</option>
+				<option value="3">Греция</option>
+			</select>
+        </div>
+		<div class="col">
+			<select class="custom-select">
+				<option selected>Дата начала тура</option>
+				<option value="1">Один</option>
+				<option value="2">Два</option>
+				<option value="3">Три</option>
+			</select>
+        </div>
+        <div class="col">
+			<select class="custom-select">
+				<option selected>Ночей</option>
+				<option value="1">7</option>
+				<option value="2">10</option>
+				<option value="3">14</option>
+			</select>
+        </div>
+        <div class="col">
+			<select class="custom-select">
+            	<option selected>Количество туристов</option>
+				<option value="1">1</option>
+				<option value="2">2</option>
+				<option value="3">2+1</option>
+			</select>
+		</div>
+	</div>
+    </form>		
