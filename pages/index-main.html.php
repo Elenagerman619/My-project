0a2fa01ece3fac803e282a90ffcbe27ee1110a93
Elenagerman619 <82209704+Elenagerman619@users.noreply.github.com>
2021-05-27 13:47:07 +0300

    Create index-main.html.php

---
+++ pages/index-main.html.php
@@ -0,0 +1,40 @@
+<div class="col1" id="img">
+	<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+		<ol class="carousel-indicators">
+			<li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+			<li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+			<li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+		</ol>
+		<div class="carousel-inner">
+			<div class="carousel-item active"><img src="/img/italy1.jpeg" class="d-block w-100" alt="...">
+				<div class="carousel-caption d-none d-md-block"><h5 style="font-size: 30px;color:whitesmoke">Италия</h5></div>
+			</div>
+			<div class="carousel-item"><img src="/img/spain.jpg" class="d-block w-100" alt="...">
+				<div class="carousel-caption d-none d-md-block"><h5 style="font-size: 30px;color:whitesmoke">Испания</h5></div>
+			</div>
+			<div class="carousel-item"><img src="/img/greece.jpg" class="d-block w-100"  alt="...">
+				<div class="carousel-caption d-none d-md-block"><h5 style="font-size: 30px;color:whitesmoke">Греция</h5></div>
+			</div>
+			<div class="carousel-item"><img src="/img/cyprus.jpg" class="d-block w-100" alt="...">
+				<div class="carousel-caption d-none d-md-block"><h5 style="font-size: 30px;color:whitesmoke">Кипр</h5></div>
+			</div>
+			<div class="carousel-item"><img src="/img/ejfeleva.jpg" class="d-block w-100" alt="...">
+				<div class="carousel-caption d-none d-md-block"><h5 style="font-size: 30px;color:whitesmoke">Франция</h5></div>
+			</div>
+			<div class="carousel-item"><img src="/img/portug.jpg" class="d-block w-100" alt="...">
+				<div class="carousel-caption d-none d-md-block"><h5 style="font-size: 30px;color:whitesmoke">Португалия</h5></div>
+			</div>
+			<div class="carousel-item"><img src="/img/chehia.jpg" class="d-block w-100" alt="...">
+				<div class="carousel-caption d-none d-md-block"><h5 style="font-size: 30px;color:whitesmoke">Чехия</h5></div>
+			</div>
+		</div>
+		<a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only">Предыдущий</span>
+		</a>
+		<a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Следующий</span>
+		</a>
+	</div>
+</div>	
